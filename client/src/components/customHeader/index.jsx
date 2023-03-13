6d3bb61ff49117607e9c77bebc84447e4ce8a023
asimpson <adam.1.simpson@nokia.com>
2023-03-12 23:33:06 -0400

    Adding more to custom header component

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import  { ChatBubbleLeftRightIcon, PhoneIcon } from '@heroicons/react/24/solid';
 
-const CustomHeader = () => {
+const CustomHeader = ({ chat }) => {
   return (
     <div className='chat-header'>
-    <div className='flexBetween'>
-
-    </div>
+      <div className='flexbetween'>
+        <ChatBubbleLeftRightIcon className="icon-chat" />
+        <h3 className="header-text">{chat.title}</h3>
+      </div>
+      <div className='flexbetween'>
+        <PhoneIcon className="icon-phone" /> 
+        <p className="header-text">{chat.desscription}</p>
+      </div>
     </div>
   )
 }
