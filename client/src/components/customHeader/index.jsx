490523330360e1921adebac407c1ad84e217d828
asimpson <adam.1.simpson@nokia.com>
2023-03-12 22:44:02 -0400

    Intiial commit

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  { ChatBubbleLeftRightIcon, PhoneIcon } from '@heroicons/react/24/solid';
+
+const CustomHeader = () => {
+  return (
+    <div className='chat-header'>
+    <div className='flexBetween'>
+
+    </div>
+    </div>
+  )
+}
+
+export default CustomHeader
